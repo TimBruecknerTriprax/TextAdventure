8d21fbc9f145500e28d190768a3faf4db4553a77
Tim Brückner <31614990+TimBruecknerTriprax@users.noreply.github.com>
2017-09-20 21:23:18 +0200

    Master Version (German)
    Originale Version des Beispiels mit deutscher Übersetzung

---
+++ Master/CommandWords.java
@@ -0,0 +1,36 @@
+/**
+ * Liste der Befehle
+ * @author (Max, Noah, Tim) 
+ * @version (0.0.1 04.09.2017)
+ */
+
+public class CommandWords
+{
+    // Ein Array zum überprüfen der Befehle (Keine Erstellung, nur Überprüfung)
+    private static final String[] validCommands = {
+        "gehe", "quit", "hilfe"
+    };
+
+    /**
+     * Initialisiert Befehle mit Werten.
+     */
+    public CommandWords()
+    {
+        // nothing to do at the moment...
+    }
+
+    /**
+     * Überprüft das String, ob ein Befehl vorhanden ist. 
+     * @return true wenn der Befehl existiert,
+     * false wenn er nicht existiert.
+     */
+    public boolean isCommand(String aString)
+    {
+        for(int i = 0; i < validCommands.length; i++) {
+            if(validCommands[i].equals(aString))
+                return true;
+        }
+        // Wenn die Schleife durchläuft ohne etwas zu finden existiert der Befehl nicht. Deshalb wird false zurückgegeben
+        return false;
+    }
+}
