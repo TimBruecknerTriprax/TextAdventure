12403c575c228e2bf3851f6818846e3df8c7f870
Tim Brückner <tim.brueckner@cityweb.de>
2017-09-20 23:39:57 +0200

    First function Character select
    -Einfache Charakter Abfrage
    - Anfang des Combat Modes

---
+++ CharakterSelect/Hauptcharakter.java
@@ -0,0 +1,22 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Hauptcharakter.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Hauptcharakter
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int pGeschwindigkeit;
+
+    /**
+     * Konstruktor für Objekte der Klasse Hauptcharakter
+     */
+    public Hauptcharakter()
+    {
+        // Instanzvariable initialisieren
+        pGeschwindigkeit=100;
+    }
+
+}
